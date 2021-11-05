36d50ae36ce94e11053985a1fbdfda889d91f9be
Cléa <80284454+Cpasklaire@users.noreply.github.com>
2021-11-05 20:08:35 +0100

    remise backend

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+/*communication avec le serveur*/
+const express = require('express');
+const mongoose = require('mongoose'); //application MongoDB
+const app = express(); /*application Express*/
+require('dotenv').config(); //application .env
+
+/*connection à MongoDB*/
+
+mongoose.connect(process.env.CONNECT_DATA_BASE,
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie'))
+  .catch(() => console.log('Connexion à MongoDB échouée'));
+
+
+/*middleware communication des différent port(localhost) possible*/
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); //accéder à notre API depuis n'importe quelle origine
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); //ajouter les headers mentionnés aux requêtes envoyées vers notre API
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //envoyer des requêtes avec les méthodes mentionnées
+  next();
+});
+
+/*parle en json*/
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const path = require('path');
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+const saucesRoutes = require('./routes/sauces');
+app.use('/api/sauces', saucesRoutes);
+
+const userRoutes = require('./routes/user');
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,47 @@
+/*créer un serveur Node*/
+const http = require('http'); //package HTTP natif de Node
+const app = require('./app');
+
+const normalizePort = val => { //normalizePort : renvoie un port valide
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {return val;}
+  if (port >= 0) {return port;}
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => { //errorHandler : recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur 
+  if (error.syscall !== 'listen') {throw error;}
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+/*création docier images*/
+const mkdirp = require('mkdirp');
+mkdirp('./images').then(made =>
+console.log(`création du dossier images`));
+
+server.listen(port); //ce démarre avec 'node server' ou 'nodemon server' 
